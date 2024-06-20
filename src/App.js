692d07790b62e18486181a84a3d2506e71722732
Ashhish-h <kushwaha.ashish2611@gmail.com>
2024-06-20 16:27:52 +0530

    create responsive Navbar

--- src/App.js
+++ src/App.js
@@ -13,7 +13,37 @@ import {
 function App() {
   return (
     <>
-      
+      <Navbar bg="light" expand="lg" clasName="mb-4">
+        <Container>
+          <Navbar.Brand href="/" className="fw-bold fs-4">News-App</Navbar.Brand>
+
+          <Navbar.Toggle aria-controls="navbar-nav" />
+
+          <Navbar.Collapse id="navbar-nav">
+            <Nav className="me-auto">
+
+              <Dropdown>
+                <Dropdown.Toggle variant="outline-primary">Categories</Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                  <Dropdown.Item>World</Dropdown.Item>
+                  <Dropdown.Item>business</Dropdown.Item>
+                  <Dropdown.Item>technology</Dropdown.Item>
+                  <Dropdown.Item>sports</Dropdown.Item>
+                  <Dropdown.Item>entertainment</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+
+            </Nav>
+
+            <Form className="d-flex">
+              <FormControl type="text" placeholder="search" className="me-2" name="search" />
+
+              <Button variant="outline-primary" type="submit">Submit</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </>
   );
 }
