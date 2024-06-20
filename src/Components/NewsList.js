2e5c2baa7820a791054c66d1ecce9191e159e554
Ashhish-h <kushwaha.ashish2611@gmail.com>
2024-06-20 21:11:05 +0530

    return container that shows news articles

--- src/Components/NewsList.js
+++ src/Components/NewsList.js
@@ -8,7 +8,7 @@ const NewsList = (props) =>{
 
     useEffect( () => {
         const fetchNews = async () => {
-            let url = `https://gnews.io/api/v4/top-headlines?lang=en`;
+            let url = "https://gnews.io/api/v4/top-headlines?category=general";
 
             if(category){
                 url += `&topic=${category}`;
@@ -25,9 +25,31 @@ const NewsList = (props) =>{
         };
 
         fetchNews();
-    });
+    }, [searchTerm, category]);
 
-    return <></>
+    return (
+        <Container>
+        <Row>
+            {news?.map((article) => (
+                <Col xs={12} md={6} lg={4} key={article.url}>
+                    <Card>
+                       <Card.Img src={article.image} variant="top"/>
+
+                       <Card.Body>
+                            <Card.Title>{article.title}</Card.Title>
+
+                            <Card.Text>{article.description}</Card.Text>
+
+                            <Card.Link href={article.url}>Read More</Card.Link>
+                       </Card.Body>
+
+                    </Card>
+                </Col>
+            ))};
+        </Row>
+    </Container>
+    );
+    
 }
 
 export default NewsList;
